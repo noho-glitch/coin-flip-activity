fe11bc09340c1296163520d96d9cc627dc339170
Dave Nolan <dpn.nolan@gmail.com>
2020-03-15 18:43:42 -0400

    created coincontainer component

---
+++ src/CoinContainer.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class CoinContainer extends Component {
+    static defaultPros = {
+        coins: [
+            { side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
+            { side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg" }
+        ]
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            currCoin: null,
+            nFlips: 0,
+            nHeads: 0,
+            nTails: 0
+        }
+    }
+    render() {
+        return (
+          <div className='CoinContainer'>
+            <h2>Let's Flip A Coin!</h2>
+            <p>
+              Out of {this.state.nFlips} flips, there have been {this.state.nHeads}{" "}
+              heads and {this.state.nTails} tails.
+            </p>
+          </div>
+        );
+      }
+}
+
+export default CoinContainer;
