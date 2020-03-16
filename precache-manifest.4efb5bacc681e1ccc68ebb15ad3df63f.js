6749bbe36d09b6a7a24697080e28177a0ca749bc
Dave Nolan <dpn.nolan@gmail.com>
2020-03-16 11:53:04 -0400

    Updates

---
+++ precache-manifest.4efb5bacc681e1ccc68ebb15ad3df63f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f68cd849a2166366b8d6f744bb3ad2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "f205dec0dd63e26337f2",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61eebb50815e1662024e",
+    "url": "/react-gh-pages/static/js/2.f3018c10.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-gh-pages/static/js/2.f3018c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f205dec0dd63e26337f2",
+    "url": "/react-gh-pages/static/js/main.abfbcfe4.chunk.js"
+  },
+  {
+    "revision": "4df48a7c0f5af939ed44",
+    "url": "/react-gh-pages/static/js/runtime-main.9da04835.js"
+  }
+]);
